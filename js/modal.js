c45d2080b7039944dd7f0d1ccbd56f77cde7002a
Matvii Karpenko <mtw.karp@gmail.com>
2019-03-13 19:32:17 +0100

    master

--- js/modal.js
+++ js/modal.js
@@ -9,15 +9,24 @@ let albums = {
   oldStyle: ["images/oldPhotosBad/1.jpg", "images/oldPhotosBad/2.jpg", "images/oldPhotosBad/3.jpg", "images/oldPhotosBad/4.jpg", "images/oldPhotosBad/5.jpg", "images/oldPhotosBad/17.jpg", "images/oldPhotosBad/7.jpg", "images/oldPhotosBad/8.jpg", "images/oldPhotosBad/9.jpg", "images/oldPhotosBad/10.jpg", "images/oldPhotosBad/19.jpg", "images/oldPhotosBad/12.jpg", "images/oldPhotosBad/18.jpg", "images/oldPhotosBad/14.jpg", "images/oldPhotosBad/15.jpg", "images/oldPhotosBad/19.jpg", "images/oldPhotosBad/20.jpg", "images/oldPhotosBad/21.jpg", "images/oldPhotosBad/22.jpg", "images/oldPhotosBad/23.jpg", "images/oldPhotosBad/24.jpg"],
   old: ["images/film/1.jpg", "images/film/2.jpg", "images/film/3.jpg", "images/film/4.jpg", "images/film/5.jpg", "images/film/17.jpg", "images/film/7.jpg", "images/film/8.jpg", "images/film/9.jpg", "images/film/10.jpg", "images/film/19.jpg", "images/film/12.jpg", "images/film/18.jpg", "images/film/14.jpg", "images/film/15.jpg", "images/film/19.jpg", "images/film/20.jpg", "images/film/21.jpg", "images/film/22.jpg", "images/film/23.jpg", "images/film/24.jpg"]
 }
+
 let bigPic = {
   block: document.getElementById('myModal'),
   picture: document.getElementById("img01"),
   span: document.querySelector('.close')
 }
 
+let goodQuality = {
+  future: ["images/fashion/1.jpg", "images/fashion/2.jpg", "images/fashion/3.jpg", "images/fashion/4.jpg", "images/fashion/5.jpg", "images/fashion/17.jpg", "images/fashion/7.jpg", "images/fashion/8.jpg", "images/fashion/9.jpg", "images/fashion/10.jpg", "images/fashion/19.jpg", "images/fashion/12.jpg", "images/fashion/18.jpg", "images/fashion/14.jpg", "images/fashion/15.jpg", "images/fashion/19.jpg", "images/fashion/20.jpg", "images/fashion/21.jpg", "images/fashion/22.jpg", "images/fashion/23.jpg", "images/fashion/24.jpg", "images/fashion/25.jpg" ],
+  past: ["images/oldPhotos/1.jpg", "images/oldPhotos/2.jpg", "images/oldPhotos/3.jpg", "images/oldPhotos/4.jpg", "images/oldPhotos/5.jpg", "images/oldPhotos/17.jpg", "images/oldPhotos/7.jpg", "images/oldPhotos/8.jpg", "images/oldPhotos/9.jpg", "images/oldPhotos/10.jpg", "images/oldPhotos/19.jpg", "images/oldPhotos/12.jpg", "images/oldPhotos/18.jpg", "images/oldPhotos/14.jpg", "images/oldPhotos/15.jpg", "images/oldPhotos/19.jpg", "images/oldPhotos/20.jpg", "images/oldPhotos/21.jpg", "images/oldPhotos/22.jpg", "images/oldPhotos/23.jpg", "images/oldPhotos/24.jpg"],
+  older: ["images/film/1.jpg", "images/film/2.jpg", "images/film/3.jpg", "images/film/4.jpg", "images/film/5.jpg", "images/film/17.jpg", "images/film/7.jpg", "images/film/8.jpg", "images/film/9.jpg", "images/film/10.jpg", "images/film/19.jpg", "images/film/12.jpg", "images/film/18.jpg", "images/film/14.jpg", "images/film/15.jpg", "images/film/19.jpg", "images/film/20.jpg", "images/film/21.jpg", "images/film/22.jpg", "images/film/23.jpg", "images/film/24.jpg"]
+}
+
+
 let modal = document.querySelector('#modalBackground');
 let imgKeeper = document.querySelector('#albumModal');
 let categoriesPics = document.querySelectorAll('.imageTypes');
+let clickedAlbum
 
 
 modal.addEventListener('click', function () {
@@ -40,6 +49,7 @@ function showAlbums(images, event) {
     let newImage = document.createElement('IMG');
     newImage.src = arrElement;
     newImage.classList.add('imageGallery');
+    newImage.classList.add('hoverable')
     imgKeeper.appendChild(newImage)
   })
   modalFunctionality()
@@ -51,11 +61,13 @@ function showCategory() {
     arrElem.addEventListener('click', function () {
       if (arrElem.id == 'fashion') {
         showAlbums(albums.modern)
+        clickedAlbum = goodQuality.future;
       } else if (arrElem.id == 'oldFashion') {
         showAlbums(albums.oldStyle)
-
+        clickedAlbum = goodQuality.past;
       } else if (arrElem.id == "film") {
         showAlbums(albums.old)
+        clickedAlbum = goodQuality.older;
       }
     })
   })
@@ -67,8 +79,17 @@ window.onload = showCategory;
 function modalFunctionality() {
   document.querySelectorAll('.imageGallery').forEach(function(arrElem){
     arrElem.addEventListener('click', function() {
-      bigPic.block.style.display = "block";
-      bigPic.picture.src = this.src;
+      let reverseTargetWord = event.target.src.split("").reverse().join("").substring(0,6);
+      
+      for (let i = 0; i < clickedAlbum.length; i++){
+        let reverseArrayPhoto = clickedAlbum[i].split("").reverse().join("").substring(0, 6)
+        if (reverseTargetWord === reverseArrayPhoto){
+
+          bigPic.block.style.display = "block";
+          bigPic.picture.src = clickedAlbum[i];
+        }
+      }
+      
     })
   })
 }
@@ -78,8 +99,13 @@ bigPic.span.addEventListener('click', function() {
   
 })
 bigPic.block.addEventListener('click',function(){
-  if(event.currentTarget === bigPic.block) {
+  if(event.target === bigPic.block) {
     bigPic.block.style.display = 'none';
   
   }
 })
+
+function reverseString(str) {
+  
+  return reverseWord
+}
