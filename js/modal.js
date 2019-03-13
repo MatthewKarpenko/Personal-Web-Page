fc2ef69b3cf0bea5766aa57238b20d7f06922ce7
Matvii Karpenko <mtw.karp@gmail.com>
2019-03-13 16:56:42 +0100

    master

--- js/modal.js
+++ js/modal.js
@@ -5,9 +5,9 @@ let images = {
 }
 
 let albums = {
-  modern: ["images/fashion/1.jpg", "images/fashion/2.jpg", "images/fashion/3.jpg", "images/fashion/4.jpg", "images/fashion/5.jpg", "images/fashion/6.jpg"],
-  oldStyle: ["images/oldPhotos/1.jpg", "images/oldPhotos/2.jpg", "images/oldPhotos/3.jpg", "images/oldPhotos/4.jpg", "images/oldPhotos/5.jpg", "images/oldPhotos/6.jpg"],
-  old: ["images/film/1.jpg", "images/film/2.jpg", "images/film/3.jpg", "images/film/4.jpg", "images/film/5.jpg", "images/film/6.jpg", "images/film/7.jpg"]
+  modern: ["images/fashionBad/1.jpg", "images/fashionBad/2.jpg", "images/fashionBad/3.jpg", "images/fashionBad/4.jpg", "images/fashionBad/5.jpg", "images/fashionBad/17.jpg", "images/fashionBad/7.jpg", "images/fashionBad/8.jpg", "images/fashionBad/9.jpg", "images/fashionBad/10.jpg", "images/fashionBad/19.jpg", "images/fashionBad/12.jpg", "images/fashionBad/18.jpg", "images/fashionBad/14.jpg", "images/fashionBad/15.jpg", "images/fashionBad/19.jpg", "images/fashionBad/20.jpg", "images/fashionBad/21.jpg", "images/fashionBad/22.jpg", "images/fashionBad/23.jpg", "images/fashionBad/24.jpg", "images/fashionBad/25.jpg" ],
+  oldStyle: ["images/oldPhotosBad/1.jpg", "images/oldPhotosBad/2.jpg", "images/oldPhotosBad/3.jpg", "images/oldPhotosBad/4.jpg", "images/oldPhotosBad/5.jpg", "images/oldPhotosBad/17.jpg", "images/oldPhotosBad/7.jpg", "images/oldPhotosBad/8.jpg", "images/oldPhotosBad/9.jpg", "images/oldPhotosBad/10.jpg", "images/oldPhotosBad/19.jpg", "images/oldPhotosBad/12.jpg", "images/oldPhotosBad/18.jpg", "images/oldPhotosBad/14.jpg", "images/oldPhotosBad/15.jpg", "images/oldPhotosBad/19.jpg", "images/oldPhotosBad/20.jpg", "images/oldPhotosBad/21.jpg", "images/oldPhotosBad/22.jpg", "images/oldPhotosBad/23.jpg", "images/oldPhotosBad/24.jpg"],
+  old: ["images/film/1.jpg", "images/film/2.jpg", "images/film/3.jpg", "images/film/4.jpg", "images/film/5.jpg", "images/film/17.jpg", "images/film/7.jpg", "images/film/8.jpg", "images/film/9.jpg", "images/film/10.jpg", "images/film/19.jpg", "images/film/12.jpg", "images/film/18.jpg", "images/film/14.jpg", "images/film/15.jpg", "images/film/19.jpg", "images/film/20.jpg", "images/film/21.jpg", "images/film/22.jpg", "images/film/23.jpg", "images/film/24.jpg"]
 }
 let bigPic = {
   block: document.getElementById('myModal'),
@@ -21,10 +21,9 @@ let categoriesPics = document.querySelectorAll('.imageTypes');
 
 
 modal.addEventListener('click', function () {
-  console.log(event.target)
+
   if (event.target === modal) {
     let imgInGallery = imgKeeper.querySelectorAll('.imageGallery')
-console.log(imgInGallery)
     modal.classList.add('hidden');
     document.body.style.overflow = "";
     imgInGallery.forEach(function (arrElem) {
@@ -70,18 +69,17 @@ function modalFunctionality() {
     arrElem.addEventListener('click', function() {
       bigPic.block.style.display = "block";
       bigPic.picture.src = this.src;
-      document.body.style.overflow = "hidden"
     })
   })
 }
 
 bigPic.span.addEventListener('click', function() {
   bigPic.block.style.display = "none";
-  document.body.style.overflow = ""
+  
 })
 bigPic.block.addEventListener('click',function(){
   if(event.currentTarget === bigPic.block) {
     bigPic.block.style.display = 'none';
-    document.body.style.overflow = ""
+  
   }
 })
